a30c4da50ccc6b1d5a79f88ea5c46dceb5092f1f
marina-nenova <marinanenova89@gmail.com>
2023-12-03 13:00:13 +0200

    React Softuni Course

---
+++ 01. ReactJS-Intro-to-React-and-JSX/react-demo/src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header.jsx";
+import OurServices from "./components/OurServices.jsx";
+import About from "./components/About.jsx";
+import Why from "./components/Why.jsx";
+import Team from "./components/Team.jsx";
+import Info from "./components/Info.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <OurServices />
+      <About />
+      <Why />
+      <Team />
+      <Info />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
